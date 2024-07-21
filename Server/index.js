b5e2097dbd6085bcf000efa7acafbfcc92413f47
sanjeeb <code.sanjeeb@gmail.com>
2024-07-22 03:10:02 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://hgnews.vercel.app'],
 };
 
 app.use(cors(corsOptions));
